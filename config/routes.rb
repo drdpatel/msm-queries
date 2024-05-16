c231153999146dd1ef274f2a48fe9a8741b1ba04
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-05-16 00:48:27 +0000

    dd dt

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
-  get("/director/:the_id", { :controller => "directors", :action => "show" })
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
 
 end
